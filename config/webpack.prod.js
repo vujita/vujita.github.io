e8b5726f5d4388bbb7b7269c492d6daab2747fd4
vnguyen <Vu.Nguyen@infor.com>
2016-07-26 03:46:12 -0500

    Initial webpack build in place

---
+++ config/webpack.prod.js
@@ -0,0 +1,41 @@
+/**
+ * Created by vnguyen on 7/26/16.
+ */
+var webpack = require('webpack');
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var ManifestPlugin = require('webpack-manifest-plugin');
+var Visualizer = require('webpack-visualizer-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+
+module.exports = webpackMerge(commonConfig, {
+    debug: false,
+
+    output: {
+        path: helpers.root('dist'),
+        publicPath: '/',
+        filename: '[name].[hash].js',
+        chunkFilename: '[id].[hash].chunk.js'
+    },
+
+    htmlLoader: {
+        minimize: false // workaround for ng2
+    },
+
+    plugins: [
+        new ManifestPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin('[name].[hash].css'),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'ENV': JSON.stringify(ENV)
+            }
+        }),
+        new Visualizer()
+    ]
+});
---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+/**
+ * Created by vnguyen on 7/26/16.
+ */
+module.exports = require('./config/webpack.dev.js');

8c88c6e5834aed48400fcf71287e771f533306f2
Vu Nguyen <Vu Nguyen>
2016-08-17 23:05:29 -0500

    Added fix for files required to gh-pages to be copied over

--- publishGhPages.js
+++ publishGhPages.js
@@ -2,11 +2,25 @@
  * Created by vuthasone on 8/17/2016.
  */
 var path = require('path'),
-    ghPages = require('gh-pages');
-ghPages.publish(path.join(__dirname, 'dist'), function (err) {
+    fs = require('fs'),
+    ghPages = require('gh-pages'),
+    distFolder = 'dist';
+//We need to move the 404 page in too the dist folder too
+copyFilesToDist(['404.html', 'CNAME', 'README.md']);
+
+//Publish dist folder to gh-pages branch
+ghPages.publish(path.join(__dirname, distFolder), function (err) {
     if (err) {
         console.log('publish error occurred', err);
     } else {
         console.log('publish sucessful');
     }
 });
+
+function copyFilesToDist(files) {
+    files.forEach(function (f) {
+        console.log('coping file', f, ' over to dist');
+        var contents = fs.readFileSync(f).toString();
+        fs.writeFileSync(distFolder + '/' + f, contents);
+    });
+}

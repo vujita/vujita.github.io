093719dc3a5ab8497561127faa3a384c722a5b0b
Vu Nguyen <Vu Nguyen>
2016-08-18 02:56:38 -0500

    Filled out an about page, hopefully not to lame. Probably address it again later

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,13 +4,30 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import style from './about.scss';
-
+import fatNinjaPic from '../../../images/fat-ninja.svg';
+const greeting = 'Hello, I am Vu';
 
 export default class About extends Component {
     render() {
         return (
             <div>
-                <h1>About me....</h1>
+                <h1 className={style.greeting}>{greeting}</h1>
+                <div className={style.whatAmI}>
+                    <p>
+                        I am a code...
+                        <img src={fatNinjaPic} className={style.ninjaPic}/>
+                        for <span className={style.money}>hire</span>
+                    </p>
+                    <p>
+                        My weapons of choice HTML, CSS, and JavaScript
+                    </p>
+                    <p>
+                        I have also been known to assault Java and .NET backends
+                    </p>
+                </div>
+                <div className={style.whereIamFrom}>
+                    <p>I come from the Lone Star State, but I am looking to move to The Golden State and have my heart set on the Bay area</p>
+                </div>
             </div>
         )
     }

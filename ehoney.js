5fa2ac03f742170a01b17c507b93298496d16176
Vu Nguyen <shardphoneix@gmail.com>
2017-01-16 20:56:47 -0800

    Update ehoney.js

--- ehoney.js
+++ ehoney.js
@@ -1,33 +1,35 @@
 /**
  * Created by vujita on 12/14/16.
  */
-setTimeout(function () {
+(function(){
     var sec = 1000;
-    checkStuff();
-    setInterval(checkStuff, 10* sec)
-    function checkStuff() {
-        console.log('Doing fun stuff to jwplayer if it is there');
-        if (typeof (jwplayer)!=='undefined' && jwplayer && jwplayer()) {
-            console.log('jwplayer found');
-            var state = jwplayer().getState();
-            console.log('jwplayer state:', state);
-            if (state === 'playing') {
-                console.log('since playing, seeking');
-                var duration = jwplayer().getDuration() || 123123123123123;
-                jwplayer().seek(duration*1000);
-                console.log("Ask for ad to start playing");
-                jwplayer().playAd();
-            }else if(state === 'complete'){
-                console.log('Player things it is complete, start playing again');
-                window.location.reload();
+    setTimeout(function () {
+        checkStuff();
+        setInterval(checkStuff, 10* sec)
+        function checkStuff() {
+            console.log('Doing fun stuff to jwplayer if it is there');
+            if (typeof (jwplayer)!=='undefined' && jwplayer && jwplayer()) {
+                console.log('jwplayer found');
+                var state = jwplayer().getState();
+                console.log('jwplayer state:', state);
+                if (state === 'playing') {
+                    console.log('since playing, seeking');
+                    var duration = jwplayer().getDuration() || 123123123123123;
+                    jwplayer().seek(duration*1000);
+                    console.log("Ask for ad to start playing");
+                    jwplayer().playAd();
+                }else if(state === 'complete'){
+                    console.log('Player things it is complete, start playing again');
+                    window.location.reload();
+                }
             }
         }
-    }
-}, 3* sec);
-setTimeout(function () {
-    window.location.reload();
-}, 2 * 60 * 1000)
-setTimeout(function(){
-    $('#navlogin').click()
-    $('#regorlogdiv').click()
-},3000)
+    }, 3 * sec );
+    setTimeout(function () {
+        window.location.reload();
+    }, 2 * 60 * 1000)
+    setTimeout(function(){
+        $('#navlogin').click()
+        $('#regorlogdiv').click()
+    },3000)
+})()

adc6f7b83f7f0cf5c91d2ee9831ad9e9d021eea6
Vu Nguyen <shardphoneix@gmail.com>
2017-01-25 20:04:37 -0800

    Update ehoney.js

--- ehoney.js
+++ ehoney.js
@@ -3,7 +3,10 @@
  */
 (function(){
     var sec = 1000;
-    if(/therapy_duck_as_service_animal_/.test(window.location.href)){
+    if(
+        /therapy_duck_as_service_animal_/.test(window.location.href) ||        
+        /uk_robo_chef_p4/.test(window.location.href)
+      ){
      window.location.href = 'http://www.earnhoney.com/en/videos/';
     }
     setTimeout(function () {

414ea2136ab28fd60b31d24e5ea4e3cff35261f5
Vu Nguyen <shardphoneix@gmail.com>
2017-04-02 11:23:42 -0700

    Update ehoney.js

--- ehoney.js
+++ ehoney.js
@@ -4,26 +4,28 @@
 (function(){
     var sec = 1000;
     var loc = window.location.href;
-    if(
-        /therapy_duck_as_service_animal_/.test(loc) ||        
-        /uk_robo_chef_p4/.test(loc) || 
-        /egyptian_wild_animal_trainer_/.test(loc) ||
-        /how_to_cook_shrimp_gyoza/.test(loc) ||
-        /baby_animals_p3/.test(loc) ||
-        /three_easy_and_healthy_juice_options/.test(loc) ||
-        /gina/.test(loc) || 
-        /mince_pie_recipe/.test(loc) ||
-        /ufc_dan_henderson/.test(loc) ||
-        /calia_by_carrie_underwood/.test(loc) ||
-        /tom_hanks_on_new_film_inferno/.test(loc) ||
-        /penguin_wetsuit/.test(loc) ||
-        /celebrities_weigh_in_on_presidential_debate/.test(loc) ||
-        /rob_lowe_interview/.test(loc) || 
-        /puppy_bowl_xiii_highlights/.test(loc) || 
-        /backstreet_boys/.test(loc) ||
-        /_curry_/.test(loc) ||
-        /gigi_hadid/.test(loc)
-      ){
+    var skipIfMatch = [
+        'therapy_duck_as_service_animal_',
+        'uk_robo_chef_p4',
+        'egyptian_wild_animal_trainer_',
+        'how_to_cook_shrimp_gyoza',
+        'baby_animals_p3',
+        'three_easy_and_healthy_juice_options',
+        'gina',
+        'mince_pie_recipe',
+        'ufc_dan_henderson',
+        'calia_by_carrie_underwood',
+        'tom_hanks_on_new_film_inferno',
+        'penguin_wetsuit',
+        'celebrities_weigh_in_on_presidential_debate',
+        'rob_lowe_interview',
+        'puppy_bowl_xiii_highlights',
+        'backstreet_boys',
+        '_curry_',
+        'gigi_hadid',
+    ];
+    var skipRegEx = new RegExp('('+ skipIfMatch.join('|')+')');
+    if( skipRegEx.test(loc) ){
         jQuery('#video .videodetdiv')[0].click();
         setTimout(function(){
             window.location.href = 'http://www.earnhoney.com/en/videos/';

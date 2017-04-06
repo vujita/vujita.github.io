57a8ca225456bf022a0ad3f0390d278d49431802
Vu Nguyen <shardphoneix@gmail.com>
2017-04-05 21:10:32 -0700

    Update ehoney.js

--- ehoney.js
+++ ehoney.js
@@ -4,34 +4,37 @@
 (function(){
     var sec = 1000;
     var loc = window.location.href;
-    var skipIfMatch = [
-        'therapy_duck_as_service_animal_',
-        'uk_robo_chef_p4',
-        'egyptian_wild_animal_trainer_',
-        'how_to_cook_shrimp_gyoza',
-        'baby_animals_p3',
-        'three_easy_and_healthy_juice_options',
-        'gina',
-        'mince_pie_recipe',
-        'ufc_dan_henderson',
-        'calia_by_carrie_underwood',
-        'tom_hanks_on_new_film_inferno',
-        'penguin_wetsuit',
-        'celebrities_weigh_in_on_presidential_debate',
-        'rob_lowe_interview',
-        'puppy_bowl_xiii_highlights',
-        'backstreet_boys',
-        '_curry_',
-        'gigi_hadid',
-    ];
-    var skipRegEx = new RegExp('('+ skipIfMatch.join('|')+')');
-    if( skipRegEx.test(loc) || window.location.host !== "www.tvglee.com"){
-        jQuery('#video .videodetdiv')[0].click();
-        setTimout(function(){
-            window.location.href = 'http://www.earnhoney.com/en/videos/';
-        }, 20 * sec);
-    }
     setTimeout(checkStuff, 5 * sec );
+    setTimeout(skipPageCheck, 250 * sec);
+    function skipPageCheck(){
+        var skipIfMatch = [
+            'therapy_duck_as_service_animal_',
+            'uk_robo_chef_p4',
+            'egyptian_wild_animal_trainer_',
+            'how_to_cook_shrimp_gyoza',
+            'baby_animals_p3',
+            'three_easy_and_healthy_juice_options',
+            'gina',
+            'mince_pie_recipe',
+            'ufc_dan_henderson',
+            'calia_by_carrie_underwood',
+            'tom_hanks_on_new_film_inferno',
+            'penguin_wetsuit',
+            'celebrities_weigh_in_on_presidential_debate',
+            'rob_lowe_interview',
+            'puppy_bowl_xiii_highlights',
+            'backstreet_boys',
+            '_curry_',
+            'gigi_hadid',
+        ];
+        var skipRegEx = new RegExp('('+ skipIfMatch.join('|')+')');
+        if( skipRegEx.test(loc) || window.location.host !== "www.tvglee.com"){
+            jQuery('#video .videodetdiv')[0].click();
+            setTimout(function(){
+                window.location.href = 'http://www.earnhoney.com/en/videos/';
+            }, 20 * sec);
+        }
+    }
     function checkStuff() {
         jQuery('#callcaptcha').click();
         jQuery('#js-btn-success').click();

0976c9a9da1103d303b1d8c839327cb3fffd0e93
Vu Nguyen <shardphoneix@gmail.com>
2019-01-12 10:17:31 -0800

    npm sort

--- publishGhPages.js
+++ publishGhPages.js
@@ -1,30 +1,29 @@
-/**
- * Created by vuthasone on 8/17/2016.
- */
-var path = require('path'),
-    fs = require('fs'),
-    ghPages = require('gh-pages'),
-    distFolder = 'dist';
-//We need to move the 404 page in too the dist folder too
+/* eslint-disable no-console */
+const path = require('path');
+const fs = require('fs');
+const ghPages = require('gh-pages');
+
+
+const distFolder = 'dist';
+const copyFilesToDist = (files) => {
+  files.forEach((f) => {
+    console.log('coping file', f, ' over to dist');
+    const contents = fs.readFileSync(f).toString();
+    fs.writeFileSync(`${distFolder}/${f}`, contents);
+  });
+};
+// We need to move the 404 page in too the dist folder too
 copyFilesToDist([
   '404.html',
   'CNAME',
   'README.md',
 ]);
 
-//Publish dist folder to gh-pages branch
-ghPages.publish(path.join(__dirname, distFolder), function (err) {
-    if (err) {
-        console.log('publish error occurred', err);
-    } else {
-        console.log('publish sucessful');
-    }
+// Publish dist folder to gh-pages branch
+ghPages.publish(path.join(__dirname, distFolder), (err) => {
+  if (err) {
+    console.log('publish error occurred', err);
+  } else {
+    console.log('publish sucessful');
+  }
 });
-
-function copyFilesToDist(files) {
-    files.forEach(function (f) {
-        console.log('coping file', f, ' over to dist');
-        var contents = fs.readFileSync(f).toString();
-        fs.writeFileSync(distFolder + '/' + f, contents);
-    });
-}

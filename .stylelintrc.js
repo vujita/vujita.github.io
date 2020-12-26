9210ae413d8c548bdb72906c5e9149c13641b287
Vu Nguyen <shardphoneix@gmail.com>
2020-12-26 16:10:44 -0600

    convert prettierrc -> prettierrc.js

---
+++ .stylelintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'stylelint-config-prettier',
+    'stylelint-prettier',
+    'stylelint-config-standard',
+    'stylelint-config-sass-guidelines',
+    'stylelint-config-idiomatic-order',
+  ],
+  rules: {
+    'order/properties-alphabetical-order': null,
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
+  },
+};

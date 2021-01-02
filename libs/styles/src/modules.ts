198cd4d9651919d80c0778e028d0dbfe02802573
Vu Nguyen <shardphoneix@gmail.com>
2021-01-02 09:18:56 -0600

    change how scss with typescript

--- libs/styles/src/modules.ts
+++ libs/styles/src/modules.ts
@@ -1,4 +1,6 @@
 import cn from '@vubnguyen/classnames';
-import * as styles from './lib/module/styles.module.scss';
+import styles from './lib/module/styles.module.scss';
 
 export const classnames = cn(styles);
+
+export { ClassesType, ClassNames } from './lib/module/styles.module.scss';

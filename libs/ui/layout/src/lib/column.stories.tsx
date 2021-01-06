c7dc846bc059e565b2b717268ac3612ce630ab80
Vu Nguyen <shardphoneix@gmail.com>
2021-01-06 07:36:20 -0600

    grid story

--- libs/ui/layout/src/lib/column.stories.tsx
+++ libs/ui/layout/src/lib/column.stories.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import { Column } from './column';
-
+import { number, text, withKnobs } from '@storybook/addon-knobs';
+import { Column, NumberAttr } from './column';
+import { Container } from './container';
+import { Row } from './row';
 export default {
   component: Column,
-  title: 'Column',
+  decorators: [withKnobs],
+  title: 'ui/layout',
 };
 
-export const primary = () => {
-  return <Column>Test</Column>;
+export const Grid = () => {
+  const numRows = number('numRows', 5);
+  const numCols = number('numCols', 5);
+  const columnMarkup = text('colMarkup', '<div>Test</div>');
+  const colProp = number('col', 5);
+  const nsProp = number('ns', 8);
+
+  const rows: JSX.Element[] = [];
+  for (let r = 0; r < numRows; r++) {
+    const cols: JSX.Element[] = [];
+    for (let c = 0; c < numCols; c++) {
+      cols.push(
+        <Column
+          key={`${r}-${c}`}
+          col={colProp as NumberAttr}
+          ns={nsProp as NumberAttr}
+        >
+          <span dangerouslySetInnerHTML={{ __html: columnMarkup }} />
+        </Column>,
+      );
+    }
+    rows.push(<Row key={r}>{cols}</Row>);
+  }
+  return <Container>{rows}</Container>;
 };
